ab3de036428aa8f7afdb149ea2f198c1aaf7e873
etnom <montgomery.okdaily@gmail.com>
2018-11-08 19:19:19 -0800

    accessor function for get_motor_heat

---
+++ BrakingMCU/BrakingMCU.ino
@@ -0,0 +1,19 @@
+// Monty Choy
+// Accessor function for get_motor_heat
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
+/*
+ * return temperature of given motor
+ * Does sensor refer to a pin? 
+ * What is the relationship between sensor and motor in terms of data?
+ */
+int get_motor_heat (int sensor, int motor) {
+	return 1000;
+}
